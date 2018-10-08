3ed1ba5831a139b5a233f9a9a5531088606eebd7
zhaizhaizhai <269351523@qq.com>
2018-10-08 22:50:48 +0800

    init commit

---
+++ app/src/main/java/com/example/mrli/miniweather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mrli.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mrli on 2018/10/8.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
