2d48d975d424d8b289615c8bd2a44497e8a4f43b
zhaizhaizhai <269351523@qq.com>
2018-10-14 21:00:36 +0800

    med project

--- app/src/main/java/com/example/mrli/miniweather/MainActivity.java
+++ app/src/main/java/com/example/mrli/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.mrli.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import zhaizhaizhai.util.NetUtil;
 
 /**
  * Created by mrli on 2018/10/8.
@@ -11,5 +15,14 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if (NetUtil.getNetworkState(this)!=NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络ok");
+            Toast.makeText(MainActivity.this,"网络ok！",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Log.d("myWeather","网络挂了！");
+            Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+        }
     }
 }
